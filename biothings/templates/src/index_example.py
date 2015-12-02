a98cf732a80628147d5a734c523c8e6cdae37204
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2015-12-02 07:36:11 -0500

    Restructured repo, renamed stuff, starting templating using python's string template

--- biothings/templates/src/index_example.py
+++ biothings/templates/src/index_example.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Simple template example used to instantiate a new biothing API
 from mybiothing.www.index_base import main
-from myexample.settings.example_settings import MyExampleSettings
+from $src_package.settings.$src_package_settings import $settings_name
 
-myexample_settings = MyExampleSettings()
+my{{ src_package }}_settings = MyExampleSettings()
 
 if __name__ == '__main__':
     main(myexample_settings.return_applist())
