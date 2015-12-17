7633849620128e520a43fcac11b4f9ad98a8227d
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2015-12-17 07:16:45 -0800

    Added api version to applist.

--- biothings_templates/{{src_package}}/src/www/api/handlers.py
+++ biothings_templates/{{src_package}}/src/www/api/handlers.py
@@ -30,11 +30,24 @@ class MetaDataHandler(MetaDataHandler):
 
 
 def return_applist():
-    return [
-        (r"/${annotation_endpoint}/(.+)/?", ${annotation_handler_name}),
-        (r"/${annotation_endpoint}/?$$", ${annotation_handler_name}),
-        (r"/${query_endpoint}/?", ${query_handler_name}),
+    settings = config.${settings_class}()
+    ret = [
         (r"/status", StatusHandler),
         (r"/metadata", MetaDataHandler),
         (r"/metadata/fields", FieldsHandler),
     ]
+    if settings._api_version:
+        ret += [
+            (r"/" + settings._api_version + "/metadata", MetaDataHandler),
+            (r"/" + settings._api_version + "/metadata/fields", FieldsHandler),
+            (r"/" + settings._api_version + "/${annotation_endpoint}/(.+)/?", ${annotation_handler_name}),
+            (r"/" + settings._api_version + "/${annotation_endpoint}/?$", ${annotation_handler_name}),
+            (r"/" + settings._api_version + "/${query_endpoint}/?", ${query_handler_name}),
+        ]
+    else:
+        ret += [
+            (r"/${annotation_endpoint}/(.+)/?", ${annotation_handler_name}),
+            (r"/${annotation_endpoint}/?$", ${annotation_handler_name}),
+            (r"/${query_endpoint}/?", ${query_handler_name}),
+        ]
+    return ret
