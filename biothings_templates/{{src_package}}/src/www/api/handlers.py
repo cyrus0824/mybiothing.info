30117bb29c3dfcdfa3780ac42cd75cd9c16a74a9
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2015-12-17 08:28:38 -0800

    More changes.

--- biothings_templates/{{src_package}}/src/www/api/handlers.py
+++ biothings_templates/{{src_package}}/src/www/api/handlers.py
@@ -5,44 +5,44 @@ import config
 
 class ${annotation_handler_name}(BiothingHandler):
     ''' This class is for the /${annotation_endpoint} endpoint. '''
-    settings = config.${settings_class}()
-    esq = ESQuery(settings)
+    _settings = config.${settings_class}()
+    esq = ESQuery(_settings)
 
 class ${query_handler_name}(QueryHandler):
     ''' This class is for the /${query_endpoint} endpoint. '''
-    settings = config.${settings_class}()
-    esq = ESQuery(settings)
+    _settings = config.${settings_class}()
+    esq = ESQuery(_settings)
 
 class StatusHandler(StatusHandler):
     ''' This class is for the /status endpoint. '''
-    settings = config.${settings_class}()
-    esq = ESQuery(settings)
+    _settings = config.${settings_class}()
+    esq = ESQuery(_settings)
 
 class FieldsHandler(FieldsHandler):
     ''' This class is for the /metadata/fields endpoint. '''
-    settings = config.${settings_class}()
-    esq = ESQuery(settings)
+    _settings = config.${settings_class}()
+    esq = ESQuery(_settings)
 
 class MetaDataHandler(MetaDataHandler):
     ''' This class is for the /metadata endpoint. '''
-    settings = config.${settings_class}()
-    esq = ESQuery(settings)
+    _settings = config.${settings_class}()
+    esq = ESQuery(_settings)
 
 
 def return_applist():
-    settings = config.${settings_class}()
+    _settings = config.${settings_class}()
     ret = [
         (r"/status", StatusHandler),
         (r"/metadata", MetaDataHandler),
         (r"/metadata/fields", FieldsHandler),
     ]
-    if settings._api_version:
+    if _settings._api_version:
         ret += [
-            (r"/" + settings._api_version + "/metadata", MetaDataHandler),
-            (r"/" + settings._api_version + "/metadata/fields", FieldsHandler),
-            (r"/" + settings._api_version + "/${annotation_endpoint}/(.+)/?", ${annotation_handler_name}),
-            (r"/" + settings._api_version + "/${annotation_endpoint}/?$", ${annotation_handler_name}),
-            (r"/" + settings._api_version + "/${query_endpoint}/?", ${query_handler_name}),
+            (r"/" + _settings._api_version + "/metadata", MetaDataHandler),
+            (r"/" + _settings._api_version + "/metadata/fields", FieldsHandler),
+            (r"/" + _settings._api_version + "/${annotation_endpoint}/(.+)/?", ${annotation_handler_name}),
+            (r"/" + _settings._api_version + "/${annotation_endpoint}/?$", ${annotation_handler_name}),
+            (r"/" + _settings._api_version + "/${query_endpoint}/?", ${query_handler_name}),
         ]
     else:
         ret += [
